5b1c2a9c14e9cd6ffdbd400585a6f2ebce6a41e8
Sneh Patel <P2512728@my365.dmu.ac.uk>
2021-03-22 17:04:57 +0000

    Added Properties and edited tblOrder and added tblProduct to connect tblProduct Primary key to tblOrder foregin key

--- Clothes Testing/clsOrder.cs
+++ Clothes Testing/clsOrder.cs
@@ -1,9 +1,16 @@
-﻿namespace Clothes_Testing
+﻿using System;
+
+namespace Clothes_Testing
 {
     public class clsOrder
     {
         public clsOrder()
         {
         }
+
+        public int OrderID { get; internal set; }
+        public DateTime Date { get; internal set; }
+        public int CusOrderID { get; internal set; }
+        public int ProductOrderID { get; internal set; }
     }
 }
--- Clothes Testing/tstOrder.cs
+++ Clothes Testing/tstOrder.cs
@@ -12,5 +12,58 @@ namespace Clothes_Testing
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.OrderID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void DatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnOrder.Date = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.Date, TestData);
+        }
+
+        [TestMethod]
+        public void CusOrderIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.CusOrderID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.CusOrderID, TestData);
+        }
+
+        [TestMethod]
+        public void ProductOrderIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnOrder.ProductOrderID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnOrder.ProductOrderID, TestData);
+        }
     }
+
 }
