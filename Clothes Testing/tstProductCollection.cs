f43418cc961619810b10a07298d45140a61a5140
Philip Allotey <alloteyphilip@gmail.com>
2021-03-26 11:12:03 +0000

    collection

---
+++ Clothes Testing/tstProductCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clothes_Testing
+{
+    [TestClass]
+    public class tstProductCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
